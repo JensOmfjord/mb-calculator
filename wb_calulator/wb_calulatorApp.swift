2aa5c06ffc2fd68d90f249b114f8a41f3332c5af
Jens Omfjord <jens.omfjord@gmail.com>
2024-11-02 13:26:57 +0100

    Initial Commit

---
+++ wb_calulator/wb_calulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  wb_calulatorApp.swift
+//  wb_calulator
+//
+//  Created by jens.omfjord on 02/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct wb_calulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ wb_calulatorTests/wb_calulatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  wb_calulatorTests.swift
+//  wb_calulatorTests
+//
+//  Created by jens.omfjord on 02/11/2024.
+//
+
+import Testing
+@testable import wb_calulator
+
+struct wb_calulatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
